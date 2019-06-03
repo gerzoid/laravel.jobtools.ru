b7e23a84e060c62480828e144c83368b2b6829c4
Gerz <gerztrue@gmail.com>
2019-06-03 22:18:19 +0800

    ЛОг запросов в БД

---
+++ database/migrations/2019_06_01_042253_create_log_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('time');
+            $table->integer('duration');
+            $table->string('ip',100)->nullabe();
+            $table->string('url')->nullabe();
+            $table->string('method',10)->nullabe();
+            $table->string('input')->nullabe();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['api']], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
